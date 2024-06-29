3fb1f0bf79d1d1065bae794d63f1ccdb67a03dfa
ariasortez <juan.arias@unitec.edu>
2024-06-28 22:12:40 -0600

    Origin Option added to the check service

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -4,20 +4,34 @@ export enum LogSeverityLevel {
   high = 'high',
 }
 
+export interface LogEntityOptions {
+  message: string;
+  level: LogSeverityLevel;
+  createdAt?: Date;
+  origin: string;
+}
+
 export class LogEntity {
   public level: LogSeverityLevel;
   public message: string;
-  public createdAt: Date;
+  public createdAt?: Date;
+  public origin: string;
 
-  constructor(message: string, level: LogSeverityLevel) {
-    this.message = message;
-    this.level = level;
+  constructor(options: LogEntityOptions) {
+    this.message = options.message;
+    this.level = options.level;
     this.createdAt = new Date();
+    this.origin = options.origin;
   }
 
   static fromJson = (json: string): LogEntity => {
-    const { message, level, createdAt } = JSON.parse(json);
-    const log = new LogEntity(message, level);
+    const { message, level, createdAt, origin } = JSON.parse(json);
+    const log = new LogEntity({
+      message,
+      level,
+      createdAt,
+      origin,
+    });
     log.createdAt = new Date(createdAt);
     return log;
   };
