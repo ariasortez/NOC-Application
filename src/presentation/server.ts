fd940fb5fae1ed7c65bd0f53cb736acca6ae4d36
ariasortez <juan.arias@unitec.edu>
2024-06-29 15:51:25 -0600

    Email service implementation

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,6 +2,7 @@ import { CheckService } from '../domain/use-cases/checks/check-service';
 import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.respository';
 import { CronService } from './cron/cron-service';
+import { EmailService } from './email/email.service';
 
 const fileSystemRepository = new LogRepositoryImpl(new FileSystemDataSource());
 
@@ -11,12 +12,29 @@ export class Server {
   public static start() {
     console.log('Server Started');
 
-    CronService.createJob('*/5 * * * * *', () => {
-      new CheckService(
-        fileSystemRepository,
-        () => console.log('Success'),
-        (error) => console.log(error)
-      ).execute('http://localhost:3000');
+    const emailService = new EmailService();
+
+    emailService.sendEmail({
+      to: 'juanariasortez@gmail.com',
+      subject: 'Testing',
+      htmlBody: `
+      
+      <h3> Logs de Sistema NOC</h3>
+      <p>
+      
+      Texto
+      </p>
+      <p>Ver logs adjuntos</p>
+      
+      `,
     });
+
+    // CronService.createJob('*/5 * * * * *', () => {
+    //   new CheckService(
+    //     fileSystemRepository,
+    //     () => console.log('Success'),
+    //     (error) => console.log(error)
+    //   ).execute('http://localhost:3000');
+    // });
   }
 }
